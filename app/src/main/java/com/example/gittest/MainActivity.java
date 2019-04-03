8300ef52aa300c3a9e237da6355c129de71e054a
lishuo <654195671@qq.com>
2019-04-03 11:36:40 +0800

    测试revert

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     setContentView(R.layout.activity_main);
-
+    //测试revert
     
   }
 }
